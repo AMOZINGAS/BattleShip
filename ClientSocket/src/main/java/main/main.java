5a9d048220dab1f2435cc80fae60ebb074e43cb1
AMOZINGAS <amosheli2004@gmail.com>
2025-03-31 17:03:03 -0700

    Se agregaron las clases bases

---
+++ ClientSocket/src/main/java/main/main.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package main;
+
+import mvp.menuFR;
+
+/**
+ *
+ * @author CISCO
+ */
+public class main {
+    
+    public static void main(String[] args){
+        
+        menuFR menu = new menuFR();
+        menu.setVisible(true);
+        
+        
+    }
+    
+}
---
+++ ServerSocket/src/main/java/com/mycompany/serversocket/ServerSocket.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.serversocket;
+
+/**
+ *
+ * @author CISCO
+ */
+public class ServerSocket {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
